3ca42c1f6761c94723953b2c1b97297acf84b7ba
Boris Mund <boris17527684@gmail.com>
2025-09-21 20:59:54 +0300

    test

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -1,10 +1,27 @@
 'use strict';
 
 /***
+ 
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let count = 0;
+
+  function makeAdder(...args) {
+    if (args.length > 0) {
+      count += args[0];
+
+      return makeAdder;
+    } else {
+      const result = count;
+
+      count = 0;
+
+      return result;
+    }
+  }
+
+  return makeAdder;
 }
 
 module.exports = makeInfinityAdder;
